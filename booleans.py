1d8bd39d87857cefa9f089a5e3204d236b73a9a3
Bayandiyevalibek <a_bayandiyev@kbtu.kz>
2025-01-28 23:03:29 +0500

    2lab

---
+++ booleans.py
@@ -0,0 +1,27 @@
+def myFunction() :
+  return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
+
+
+x = 200
+print(isinstance(x, int))
+
+
+class myclass():
+  def __len__(self):
+    return 0
+
+myobj = myclass()
+print(bool(myobj))
+
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+
+
